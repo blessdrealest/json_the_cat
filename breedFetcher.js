0de883c03bfd026b4fc67b7b1203a73fd1d05a5f
Blessing <dijiblessing2@hotmail.co.uk>
2023-06-27 00:43:31 -0600

    code has been cleaned and linted. Comments removed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,34 +1,34 @@
-const request = require ('request');
+const request = require('request');
 
 
 
 const breedFetcherData = () => {
-const url = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
+  const url = 'https://api.thecatapi.com/v1/breeds/search?q=sib';
 
-request.get(url, (error, response,  body) => {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    
-  const data = JSON.parse(body); //convert string to obj
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+    } else {
 
-  if (data.length > 0) {
-    const firstEntry = data[0];
-    console.log('Description:', firstEntry.description);
-  } else {
-    console.log('No breeds found.');
-  }
+      const data = JSON.parse(body);
 
-  }
-});
+      if (data.length > 0) {
+        const firstEntry = data[0];
+        console.log('Description:', firstEntry.description);
+      } else {
+        console.log('No breeds found.');
+      }
 
-}
+    }
+  });
 
-const breedName = process.argv[2]; // breed name as 3rd argument represents name
+};
+
+const breedName = process.argv[2];
 
 if (breedName) {
   breedFetcherData(breedName);
 } else {
-  console.log('Breed Name not present'); // case of if no name/incorrect name given
+  console.log('Breed Name not present');
 }
 
