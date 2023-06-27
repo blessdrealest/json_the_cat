652e0c1e8884188927d18b5fd3ce2b14c3864e03
Blessing <dijiblessing2@hotmail.co.uk>
2023-06-27 17:46:05 -0600

    Added mocha, chai tests. Ensured test success. Removed console.log from breedFetcher.js to utilize async callback. refactored code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,19 @@
 const request = require('request');
 
-
-
 const fetchBreedDescription = (breedName, callback) => {
-  const url = 'https://api.thecatapi.com/v1/breeds/search?q=${breedName}';
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-  request.get(url, (error, response, body) => {
+  request(url, (error, response, body) => {
     if (error) {
-      console.error('Request Error:', error);
+      callback(error);
     } else {
       const data = JSON.parse(body);
 
       if (data.length > 0) {
         const firstEntry = data[0];
-        console.log('Description:', firstEntry.description);
+        callback(null, firstEntry.description);
       } else {
-        console.log('No breeds found.');
+        callback('No breeds found.');
       }
 
     }
@@ -23,12 +21,5 @@ const fetchBreedDescription = (breedName, callback) => {
 
 };
 
-const breedName = process.argv[2];
-
-if (breedName) {
-  fetchBreedDescription(breedName);
-} else {
-  console.log('Breed Name not present');
-}
 
 module.exports = { fetchBreedDescription };
